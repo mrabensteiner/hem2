3db48d46df9491594e7c0bd373bc99888750b339
Martin Rabensteiner <martin.rabensteiner@student.tugraz.at>
2026-08-28 14:42:48 +0200

    vcs files

--- api/lib/prisma.ts
+++ api/lib/prisma.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../generated/prisma/client";
-import { loadEnvFile } from 'node:process';
+import { PrismaClient } from "../prisma/generated/client";
+import { loadEnvFile } from "node:process";
 import bcrypt from 'bcrypt';
 
 loadEnvFile('./../.env');
