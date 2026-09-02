ce64535251949902ad8dd4b60fce1a371a5827bd
Martin Rabensteiner <martin.rabensteiner@student.tugraz.at>
2026-09-02 07:54:26 +0200

    inline search box prototype

--- ui/src/modules/projects/EditView.vue
+++ ui/src/modules/projects/EditView.vue
@@ -32,6 +32,7 @@ async function save() {
 }
 
 const edited = ref<boolean>(false);
+const searchManager = ref<string>(" ");
 </script>
 
 
@@ -82,12 +83,22 @@ const edited = ref<boolean>(false);
 
     <div>
       <label>Managers</label>
-      <div class="checkbox-group">
-        <label v-for="u in users" :key="'form-mgr-' + u.id" class="checkbox-label">
-          <input type="checkbox" :value="u.id" v-model="managers" />
-          {{ u.firstname }} {{ u.lastname }}
-        </label>
-      </div>
+      <label class="inlinesearchbox" for="managersearch">
+        <template v-for="u in users">
+          <div class="searchchip">
+            <label><input type="checkbox" :value="u.id" v-model="managers" :id="'mgm-' + u.id"/></label>
+            {{ u.firstname }} {{ u.lastname }}
+          </div>
+        </template>
+        <div>
+          <input type="search" id="managersearch" v-model="searchManager" autocomplete="off"/>
+          <div class="suggestions" v-if="searchManager != ''">
+            <label v-for="u in users.filter((uf:any) => !managers.includes(uf.id) && (uf.firstname + ' ' + uf.lastname).toLowerCase().search(searchManager.toLowerCase()) != -1)" :for="'mgm-' + u.id" tabindex="0">
+              {{ u.firstname }} {{ u.lastname }}
+            </label>
+          </div>
+        </div>
+      </label>
     </div>
 
     <div>
@@ -100,3 +111,83 @@ const edited = ref<boolean>(false);
   </form>
   <Message :success="success" :error="error" />
 </template>
+
+<style scoped>
+.inlinesearchbox {
+  display: flex;
+  flex-wrap: wrap;
+  cursor: text;
+  border: 2px solid var(--color-border);
+  background-color: var(--color-background-soft);
+  gap: .5rem;
+
+  input[type=search] {
+    border: none;
+    height: 100%;
+  }
+
+  :focus {
+    outline: none;
+  }
+
+  &:has(:focus) {
+    border-color: var(--app-primary);
+  }
+
+  &:not(:has(:focus)) .suggestions {
+    display: none;
+  }
+
+  .suggestions {
+    background-color: var(--color-background-mute);
+    width: fit-content;
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    margin-top: .5rem;
+
+    label {
+      border: 1px solid var(--color-border);
+      margin: 0;
+      font-weight: normal;
+
+      &:hover {
+        background-color: var(--color-background-soft);
+        cursor: pointer;
+      }
+    }
+  }
+
+  .searchchip {
+    background-color: var(--color-border);
+    height: 2.5rem;
+    border-radius: 1.25rem;
+    padding-right: 1rem;
+    width: fit-content;
+
+    &:has(input:not(:checked)) {
+      display: none;
+    }
+
+    label {
+      display: inline-flex;
+      background-color: var(--color-border-hover);
+      border-radius: 1.25rem;
+      height: 2.5rem;
+      width: 2.5rem;
+
+      &:hover, input:hover {
+        background-color: var(--color-border);
+        cursor: pointer;
+      }
+      &::before {
+        content: 'x';
+        margin-left: 0.5rem;
+      }
+      input[type=checkbox] {
+        opacity: 0;
+      }
+    }
+  }
+}
+</style>
